72f7e422f7a13a879c128ec7e9529e321451aa3a
thalesaguiar21 <thalesaguiar21@gmail.com>
2016-11-23 23:39:14 +0000

    Segunda remoção do arquivo .DS_Store

--- Main.cpp
+++ Main.cpp
@@ -38,7 +38,7 @@ int main () {
 	}
 	cout << "\n\n";
 
-	for (int i=0; i<1; i++) {
+	for (int i=0; i<5; i++) {
 		gettimeofday(&inicio, NULL);
 		result = gap->ReactiveGrasp (2, 1.0);
 		gettimeofday(&final, NULL);
